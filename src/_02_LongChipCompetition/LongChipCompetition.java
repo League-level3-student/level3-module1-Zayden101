6ec476ac54748c2e1cf39a802e20b9d992cf20a5
league <league@adminisorsimac3.attlocal.net>
2023-05-19 18:22:27 -0700

    yey i finished long chip

--- src/_02_LongChipCompetition/LongChipCompetition.java
+++ src/_02_LongChipCompetition/LongChipCompetition.java
@@ -4,69 +4,99 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class LongChipCompetition {
-    /*
-     * The Beatles are eating lunch and playing a game to see who has the
-     * longest chip. (In England, french fries are called "chips".)
-     * Find the Beatle with the longest chip. You may not edit the Chip or
-     * Beatle classes. Make sure to initialize The Beatles before you start
-     * your search.
-     */
-    private ArrayList<Beatle> theBeatles = new ArrayList<Beatle>();
+	/*
+	 * The Beatles are eating lunch and playing a game to see who has the
+	 * longest chip. (In England, french fries are called "chips".)
+	 * Find the Beatle with the longest chip. You may not edit the Chip or
+	 * Beatle classes. Make sure to initialize The Beatles before you start
+	 * your search.
+	 */
+	private ArrayList<Beatle> theBeatles = new ArrayList<Beatle>();
 
-    public static void main(String[] args) {
-        LongChipCompetition lcc = new LongChipCompetition();
+	public static void main(String[] args) {
+		LongChipCompetition lcc = new LongChipCompetition();
 
-    }
+		lcc.initializeBeatles();
+		double longes = 0;
+		Beatle betle = null;
 
-    private void initializeBeatles() {
-        Beatle george = new Beatle("George");
-        Beatle john = new Beatle("John");
-        Beatle paul = new Beatle("Paul");
-        Beatle ringo = new Beatle("Ringo");
-        theBeatles.add(george);
-        theBeatles.add(john);
-        theBeatles.add(paul);
-        theBeatles.add(ringo);
-    }
+		ArrayList<Beatle> beatlee = lcc.getTheBand();
 
-    public ArrayList<Beatle> getTheBand(){
-        return theBeatles;
-    }
+
+		for(Beatle b:beatlee) {
+
+			ArrayList<Chip> chipp = b.getChips();
+
+			double longestchipinthecurrentbagofchips = 0;
+
+			for(Chip c:chipp) {
+				if (c.getLength() > longestchipinthecurrentbagofchips) {
+					longestchipinthecurrentbagofchips=c.getLength();
+				}
+			}
+			if (longestchipinthecurrentbagofchips > longes) {
+				longes=longestchipinthecurrentbagofchips;
+				
+				betle=b;
+			}
+			
+		}
+		
+		System.out.println(betle.getName() + " has the longest chip, mesuring at an astonsing " + String.format("%.2f", longes) + "ft");
+	}
+
+
+	private void initializeBeatles() {
+		Beatle george = new Beatle("George");
+		Beatle john = new Beatle("John");
+		Beatle paul = new Beatle("Paul");
+		Beatle ringo = new Beatle("Ringo");
+		theBeatles.add(george);
+		theBeatles.add(john);
+		theBeatles.add(paul);
+		theBeatles.add(ringo);
+	}
+
+	public ArrayList<Beatle> getTheBand(){
+		return theBeatles;
+	}
 }
 
 class Beatle {
-    private String name;
-    private ArrayList<Chip> chips = new ArrayList<Chip>();
+	private String name;
+	private ArrayList<Chip> chips = new ArrayList<Chip>();
 
-    public Beatle(String name) {
-        this.name = name;
-        initializePlateOfChips();
-    }
+	public Beatle(String name) {
+		this.name = name;
+		initializePlateOfChips();
+	}
 
-    private void initializePlateOfChips() {
-        int numberOfChips = new Random().nextInt(100);
-        for (int i = 0; i < numberOfChips; i++) {
-            chips.add(new Chip(new Random().nextDouble() * 10));
-        }
-    }
+	private void initializePlateOfChips() {
+		int numberOfChips = new Random().nextInt(100);
+		for (int i = 0; i < numberOfChips; i++) {
+			chips.add(new Chip(new Random().nextDouble() * 10));
+		}
+	}
 
-    public ArrayList<Chip> getChips() {
-        return this.chips;
-    }
+	public ArrayList<Chip> getChips() {
+		return this.chips;
+	}
 
-    public String getName() {
-        return this.name;
-    }
+	public String getName() {
+		return this.name;
+	}
 }
 
 class Chip {
-    private double length;
+	private double length;
 
-    public double getLength() {
-        return length;
-    }
+	public double getLength() {
+		return length;
+	}
 
-    Chip(double d) {
-        this.length = d;
-    }
+	Chip(double d) {
+		this.length = d;
+	}
 }
+
+

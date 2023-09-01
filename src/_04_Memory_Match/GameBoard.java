03ffe395e93fec578da844852cd6d95406460773
league <league@adminisorsimac3.attlocal.net>
2023-09-01 16:39:05 -0700

    finally coding again

--- src/_04_Memory_Match/GameBoard.java
+++ src/_04_Memory_Match/GameBoard.java
@@ -4,6 +4,8 @@ import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -12,7 +14,7 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-import _03_RemovingStuffFromArrayLists.ArrayListRemove.Stuff;
+//import _03_RemovingStuffFromArrayLists.ArrayListRemove.Stuff;
 
 public class GameBoard extends JFrame implements ActionListener {
     private static final long serialVersionUID = 1L;
@@ -20,7 +22,7 @@ public class GameBoard extends JFrame implements ActionListener {
     static Card secondSelectedCard = null;
     
     // 1. Initialize TOTAL_CARDS to 2;
-    static int TOTAL_CARDS = 0;
+    static int TOTAL_CARDS = 2;
     
     ArrayList<Card> cards;
     
@@ -45,34 +47,54 @@ public class GameBoard extends JFrame implements ActionListener {
         
         // 2. Initialize the ArrayList of Cards declared above
         
-        
+        ArrayList<Card> cards = new ArrayList<Card>();
+
         
         // 3. Create TOTAL_CARDS number of objects each with a value of 1.
         //    Also, add action listeners to each Card object and then add each
         //    of the Card objects to the ArrayList of Cards.
         
+        for (int i = 0; i < TOTAL_CARDS; i++) {
+			        Card crad = new Card(1);
+			        cards.add(crad);
+
+		}
+        
+        
         
         // 4. Use Collections.shuffle() method to randomize the order of
         //    the cards in the ArrayList
-        
+                
+        Collections.shuffle(cards);
         
         // 5. Initialize the panel variable declared above
         
+        JPanel panel = new JPanel();
         
         // 6. Add all of the Card objects to the panel
-        
+        for (int i = 0; i < TOTAL_CARDS; i++) {
+        	panel.add(cards.get(i));
+
+		}
         
         // 7. Call the setupGui() method to set up the frame
         
+        setupGui(cards);
         
         // 8. Call the startGame() method to start the game
         
+        startGame();
+        
     }
-
+    
+    
+    
     // 9. Fill in the drawCards method to draw all the cards in the ArrayList.
     //    Run your code and verify 2 cards are displayed and the game works.
     public void drawCards() {
         
+
+    	
     }
     
     // 10. 

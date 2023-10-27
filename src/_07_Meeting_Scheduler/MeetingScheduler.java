d95696511e89036800c3879cfa98ee7ce08d9570
jereycui2008 <jeremycuid@gmail.com>
2023-10-27 16:56:32 -0700

    turning retrosun into a game

--- src/_07_Meeting_Scheduler/MeetingScheduler.java
+++ src/_07_Meeting_Scheduler/MeetingScheduler.java
@@ -1,5 +1,8 @@
 package _07_Meeting_Scheduler;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class MeetingScheduler {
     /*
      * Your task is to code a method to find a meeting time for two people
@@ -22,7 +25,8 @@ public class MeetingScheduler {
      * Assume both schedules are in the same time zones
      */
     public static Schedule getMutualAvailability(Schedule person1, Schedule person2) {
-        
+    	
+    	
         return null;
     }
 }

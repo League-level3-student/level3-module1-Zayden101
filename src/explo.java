903920c4ff1e78d697b69eeae89a8715400138e2
league <league@imac-24.attlocal.net>
2024-10-18 16:56:50 -0700

    finished end block flash, need to add enemies next

---
+++ src/explo.java
@@ -0,0 +1,23 @@
+import java.awt.Color;
+import java.util.Iterator;
+
+public class explo {
+
+	static int x = 324;
+	
+public static void main(String[] args) {
+	for (int i = 0; i < 198; i++) {
+		//System.out.println("ColorfulRectangle buildings2ChangeColorRect" + x + ";");
+		//System.out.println("rects.add(buildings2ChangeColorRect" + x + ");");
+		//System.out.println("buildings2ChangeColorRect" + x + ".col = Color.MAGENTA;");
+		//System.out.println("buildings2ChangeColorRect" + x + ".col = Color.PINK;");
+		System.out.println("buildings2ChangeColorRect" + x + ".col = Color.BLUE;");
+
+		x+=1;
+	}
+	
+}
+
+
+
+}

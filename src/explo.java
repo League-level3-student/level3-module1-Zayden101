26217b7c70cd3f60ec00894cbc954bf7f081b485
league <league@imac-24.attlocal.net>
2025-01-24 18:30:30 -0800

    working on map revealing

--- src/explo.java
+++ src/explo.java
@@ -10,15 +10,24 @@ public class explo  {
 		//x:400			  y:280
 		//88			  54
 
-		int x = -240;
-		int y = -120;
-		for (int i = 0; i < 54; i++) {
+		int x = 160;
+		int y = 160;
+		y=235;
+		y=310;
+		y=385;
+		//System.out.println(x + "   1");
+
+		for (int i = 0; i < 9; i++) {
+			//System.out.println(x + "   2");
 			for (int j = 0; j < 88; j++) {
-			System.out.println("rects.add(new ColorfulRectangle(400+" + x + ",280+" + y + ", 5, 5, Color.BLACK));");
+			System.out.println("rects.add(new ColorfulRectangle(" + x + "," + y + ", 5, 5, Color.BLACK));");
+			//System.out.println("rects.add(new ColorfulRectangle(400+" + x + ",280+" + y + ", 5, 5, Color.BLACK));");
 			x+=5;
+			//System.out.println(x + "   3");
 			}
-		x = -240;
+		x = 160;
 		y+=5;
+		//System.out.println(y);
 		}
 		
 		
